789f28061b84761342b04ee57b97fd184d9e166a
DELL5400\root <andrew.chumakov12@gmail.com>
2024-01-03 17:01:01 +0200

    добавлен лог сервис

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import { surveyController } from "./Controllers/surveyController.mjs"
-
-async function main() {
-    const descriptionObj = await surveyController() // опросник 
-    
-    console.log(descriptionObj)
-}
-
-main()
