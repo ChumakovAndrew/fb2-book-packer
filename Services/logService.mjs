a39613115de2d91c09abb12fffd8194dac43e1cc
DELL5400\root <andrew.chumakov12@gmail.com>
2024-01-03 17:05:20 +0200

    рефактоинг

--- Services/logService.mjs
+++ Services/logService.mjs
@@ -1,16 +1,13 @@
 import chalk from 'chalk';
 
-const {bgRed, bgGreen, red} = chalk;
+const {bgRed, bgGreen, red, green} = chalk;
 
 const printError = (error) => {
     console.log(bgRed(' ERROR ') + " " + red(error));
 };
 
 const printSuccess = (message) => {
-	console.log(bgGreen(' SUCCESS ') + ' ' + message);
+	console.log(bgGreen(' SUCCESS ') + ' ' + green(message));
 };
-
-
-
 
 export {printError, printSuccess}
